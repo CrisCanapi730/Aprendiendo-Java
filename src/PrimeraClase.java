c6a285c3010656484effaaeabf56364ed19f8355
Cristian Canapi <cristiancanapi730@gmail.com>
2025-09-16 23:45:20 -0400

    First primitive types

--- src/PrimeraClase.java
+++ src/PrimeraClase.java
@@ -4,5 +4,6 @@ public class PrimeraClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo!!");
+		// Datos primitivos
 	}
 }
---
+++ src/UsoVariables.java
@@ -0,0 +1,17 @@
+
+public class UsoVariables {
+
+	public static void main(String[] args) {
+		// Variables in Java
+		
+		byte age=10; // -128 a 127
+		
+		age = 1;
+		
+		var letter = 'a';
+		
+		System.out.println(age);
+		System.out.println(letter);
+	}
+
+}
